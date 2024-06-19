82f47a7c031c2e824b24511a238f4fb9b8e6d6e7
Jesse Wallace <jessewacka@hotmail.com>
2024-06-19 23:34:41 +1000

    remove newlines from fasta records

--- align-reads.sh
+++ align-reads.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Align reads in reads.fasta and put them in msa.fasta
 
-mafft --globalpair --retree 2 --maxiterate 2 --anysymbol reads.fasta > msa.fasta
+mafft --globalpair --retree 2 --maxiterate 2 --anysymbol reads.fasta | \
+  tr '\n' ' ' | sed -E 's/$/\n/;s/>(\w+) /\n>\1\n/g;s/ //g' | tail -n+2 > msa.fasta
