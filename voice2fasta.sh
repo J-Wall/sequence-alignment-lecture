a8e570939abd7717963bda3e5a35b82611e6d75c
Jesse Wallace <jessewacka@hotmail.com>
2024-06-19 23:35:26 +1000

    collapse consecutive undescores

--- voice2fasta.sh
+++ voice2fasta.sh
@@ -7,5 +7,5 @@ do
   echo '>'$n | tee -a reads.fasta
   whisper reads/$n.wav \
       --model tiny.en -f srt --output_dir .srt --condition_on_previous_text False --fp16 False \
-      | cut -f5- -d' ' | sed -E 's/([A-Z])/\L\1/g;s/ /_/g;s/[^a-z_]//g' | tee -a reads.fasta
+      | cut -f5- -d' ' | sed -E 's/([A-Z])/\L\1/g;s/ /_/g;s/[^a-z_]//g;s/__+/_/g' | tee -a reads.fasta
 done
